a953007f90b648792ce5fdd29b47a8e80cb65b3d
Danai Guruva <danishto@gmail.com>
2022-01-12 19:05:40 +0200

    Create WeatherController.php

---
+++ src/Controller/WeatherController.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Request;
+use App\Services\WeatherService;
+
+class WeatherController extends AbstractController
+{
+    /**
+     * @Route("/weather", name="weather")
+     */
+    public function index(): Response
+    {
+        return $this->render('weather/index.html.twig', [
+            'controller_name' => 'WeatherController',
+        ]);
+    }
+
+    /**
+     * @Route("/predictions/{weatherElement}/{city}", name="predictions")
+     * @param Request $request
+     * @return Response
+     */
+    public function predictions(Request $request, WeatherService $weatherService): Response
+    {
+        $cityName = $request->get('name');
+        $weatherElement = $request->get('weatherElement');
+        $response = [];
+
+        try{
+            $predictions = $weatherService->getPredictionByCity($cityName, $weatherElement);
+            if(isset($predictions)){
+                $response = $predictions;
+            }
+        }
+        catch(\Exception $e){
+            error_log($e->getMessage());
+        }
+        
+        return $this->json([
+            'predictions' => $response
+        ]); 
+    }
+}
